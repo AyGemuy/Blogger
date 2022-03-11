dd6cf724b0dc727f414647b0e25a7e179b1ae3ca
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-03-11 19:12:09 +0800

    Delete top.js

--- top.js
+++
@@ -1,7 +0,0 @@
-$(document).scroll(function() {
-  return $(document).scrollTop() &gt; 300 ? $(&#39;.BackToTop&#39;).addClass(&#39;show&#39;) : $(&#39;.BackToTop&#39;).removeClass(&#39;show&#39;)
-  }), $(&#39;.BackToTop&#39;).click(function() {
-    return $(&#39;html,body&#39;).animate({
-      scrollTop: &#39;0&#39;
-    });
-});
